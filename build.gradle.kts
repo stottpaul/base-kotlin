47f271b5e31df9f61f7688442d94b9fabb23e274
Paul Stott <pstott@wayfair.com>
2021-10-15 11:49:09 +0100

    Add coroutines and a main method

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.30"
 }
 
 group = "co.uk.tangosolutions"
@@ -12,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     testImplementation(kotlin("test"))
     testImplementation("io.kotest:kotest-assertions-core:4.0.6")
 }
